284e0e037a3d69f06cc0fa261ae960b1350b965c
shaodado <shaoshineshine@gamil.com>
2024-10-21 20:44:52 +0800

    feat(html): 改變文字樣式，利用樣式繫結

--- src/app/task-feature/task/task.component.ts
+++ src/app/task-feature/task/task.component.ts
@@ -3,16 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
+  styleUrl: './task.component.css',
 })
 export class TaskComponent {
-content="建立代辦事項元件";
+  content = '建立代辦事項元件';
 
-state:'None'|'Doing'|'Finish'='None';
-onSetState(state:'None'|'Doing'|'Finish'){
-  this.state=state;
-}
+  type: 'Home' | 'Work' | 'Other' = 'Work';
+  fontSize = 14;
 
-totalCount=10;
-finishCount=3;
+  state: 'None' | 'Doing' | 'Finish' = 'None';
+  onSetState(state: 'None' | 'Doing' | 'Finish') {
+    this.state = state;
+  }
+
+  totalCount = 10;
+  finishCount = 3;
 }
