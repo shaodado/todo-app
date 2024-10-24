dbd0ebc5112878a58d58035ff6797c1e5b6e7d50
shaodado <shaoshineshine@gamil.com>
2024-10-24 12:35:18 +0800

    feat(html): 頁面容器元件

---
+++ src/app/utils/page-container/page-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-container',
+  templateUrl: './page-container.component.html',
+  styleUrl: './page-container.component.css'
+})
+export class PageContainerComponent {
+
+}
