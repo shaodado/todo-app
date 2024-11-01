a987ff59852b2dda844d2bb5cae97f2f5bb4f2b7
shaodado <shaoshineshine@gmail.com>
2024-11-01 11:27:13 +0800

    feat(html,ts): 頁面初始變更檢測

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TaskComponent } from './task-feature/task/task.component';
+import { Task } from './task-feature/task/model/task';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +8,48 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  content = '建立代辦事項元件';
-  type: 'Home' | 'Work' | 'Other' = 'Work';
-  state: 'None' | 'Doing' | 'Finish' = 'None';
+  @ViewChild(TaskComponent)
+  viewTask!: TaskComponent;
+
+  @ViewChild(TaskComponent, { static: true })
+  staticTask!: TaskComponent;
+
+  task = new Task({
+    id: 1,
+    content: '建立待辦事項元件',
+    type: 'Work',
+    state: 'None',
+  });
 
   totalCount = 10;
+
   finishCount = 3;
 
+  ngOnInit(): void {
+    // 要用右上方的 Open in New Window 功能，利用開發者工具觀察
+    console.log('Angular ngOnInit Lift Cycle Hook - viewTask', this.viewTask);
+    console.log(
+      'Angular ngOnInit Lift Cycle Hook - staticTask',
+      this.staticTask
+    );
+  }
+
+  ngAfterViewInit(): void {
+    console.log(
+      'Angular ngAfterViewInit Lift Cycle Hook - viewTask',
+      this.viewTask
+    );
+    console.log(
+      'Angular ngAfterViewInit Lift Cycle Hook - staticTask',
+      this.staticTask
+    );
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('Angular ngAfterViewChecked Lift Cycle Hook');
+  }
+
   onSetState(state: 'None' | 'Doing' | 'Finish'): void {
-    this.state = state;
+    this.task.state = state;
   }
 }
