59b2e7b8c858dab2e7106164bbe740ca38559697
shaodado <shaoshineshine@gamil.com>
2024-11-28 16:58:37 +0800

    feact(ts): Attribute Directive 自訂變更原元素樣式屬性指令

---
+++ src/app/utils/directive/black-button.directive.ts
@@ -0,0 +1,17 @@
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: 'button[appBlackButton]',
+})
+export class BlackButtonDirective {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'black'
+    );
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+  }
+}
